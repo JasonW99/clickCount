920bfc5935b894099364e6a418900d1f28ecfde8
JasonW <ningweiw@gmail.com>
2017-11-12 23:03:29 -0800

    helloworld from html

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var express = require('express');
 
 var app = express();
 
+// app.get('/', function (req, res) {
+//     res.send('Hello world!');
+// });
+
 app.get('/', function (req, res) {
-    res.send('Hello world!');
+    res.sendFile(process.cwd() + '/index.html');
 });
 
 app.listen(3000, function () {
