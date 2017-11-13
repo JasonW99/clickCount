b3c4c9e7f7f81f107d703d8f3428a4b71a89899c
JasonW <ningweiw@gmail.com>
2017-11-12 23:27:06 -0800

    add route

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 'use strict';
 
-var express = require('express');
+
+// var express = require('express');
+
+// pass the Express application as an argument to our routes function object
+var express = require('express'),
+    routes = require('./app/routes/index.js');
 
 var app = express();
 
+routes(app);
+
+// send direct message to the browser
 // app.get('/', function (req, res) {
 //     res.send('Hello world!');
 // });
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/index.html');
-});
+// send a html page to the browser
+// app.get('/', function (req, res) {
+//     res.sendFile(process.cwd() + '/test_index.html');
+// });
+
+
 
 app.listen(3000, function () {
     console.log('Listening on port 3000...');
